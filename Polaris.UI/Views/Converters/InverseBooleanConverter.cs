03ebafbb29dbf6b96c90915b90d11e7ba64d9d10
goodguyjay <jair.ramos@live.com>
2025-07-25 00:08:25 -0300

    revamp on the ui

---
+++ Polaris.UI/Views/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Polaris.UI.Views.Converters;
+
+public sealed class InverseBooleanConverter : IValueConverter
+{
+    public object? Convert(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    ) => value is bool b ? !b : value;
+
+    public object? ConvertBack(
+        object? value,
+        Type targetType,
+        object? parameter,
+        CultureInfo culture
+    ) => value is bool b ? !b : value;
+}
